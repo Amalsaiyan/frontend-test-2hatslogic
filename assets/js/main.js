545d619c17d4fa93ffde22352f50cb57f8fb621a
Amal ks <amalks8001@gmail.com>
2025-03-04 16:53:40 +0530

    web updatd

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+let testimonialHeight = document.querySelector("main").getBoundingClientRect().height;
+console.log(testimonialHeight);
+
+if (testimonialHeight) {
+    document.documentElement.style.setProperty("--testimonial-height", `${testimonialHeight}px`);
+}
+
+
+  var swiper = new Swiper(".testimonialSwiper1", {
+    direction: "vertical",
+    slidesPerView: 3,
+    spaceBetween: 33,
+    speed: 15000,
+    autoplay: {
+      delay: 0,
+    },
+    loop: true,
+    allowTouchMove: false,
+    disableOnInteraction: true,
+  });
+
+  var swiper = new Swiper(".testimonialSwiper2", {
+    direction: "vertical",
+    slidesPerView: 3,
+    spaceBetween: 33,
+    speed: 15000,
+    autoplay: {
+      delay: 0,
+      reverseDirection: true,
+    },
+    loop: true,
+    allowTouchMove: false,
+    disableOnInteraction: true,
+  });
+});
